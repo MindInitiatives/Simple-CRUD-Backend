6001a467e4a3add0107c4228ccfd2036cae9f177
Stephen Oloto <steevyn51@gmail.com>
2022-05-30 17:23:17 +0100

    Completed the Update Method and Prepped Build for Deployment

---
+++ src/main/java/com/example/simpleCrudBackend/student/StudentConfig.java
@@ -0,0 +1,35 @@
+package com.example.simpleCrudBackend.student;
+
+
+import com.sun.tools.javac.util.List;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student mariam = new Student(
+                    "Mariam",
+                    "mariam.jamal@gmail.com",
+                    LocalDate.of(2000, Month.JANUARY, 5)
+            );
+
+            Student alex = new Student(
+                    "Alex",
+                    "alex.jamal@gmail.com",
+                    LocalDate.of(2004, Month.JANUARY, 5)
+            );
+
+            repository.saveAll(
+                    List.of(mariam, alex)
+            );
+        };
+    }
+}
---
+++ src/test/java/com/example/simpleCrudBackend/SimpleCrudBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpleCrudBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCrudBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
